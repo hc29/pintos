6af195f114ba2ecb3776209e1f82122c6ad31b61
osboxes.org <hhhcccwww@gamil.com>
2021-03-26 07:25:37 -0400

    First try at implementation of syscalls

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,8 +3,13 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
+#include "userprog/pagedir.h"
 
 static void syscall_handler (struct intr_frame *);
+void sys_exit(int status);
+void sys_write(int fd, const void* buffer, unsigned size);
+void check_address_validity(const void* vaddr);
 
 void
 syscall_init (void) 
@@ -15,6 +20,58 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+	int *p = f->esp;
+	check_address_validity(p);
+
+	switch(*p)
+	{
+		case SYS_EXIT:
+		check_address_validity(p+1);
+		sys_exit(*(p+1));
+
+		case SYS_WRITE:
+		check_address_validity(p+5);
+		check_address_validity(p+6);
+		check_address_validity(*(p+6));
+		check_address_validity(p+7);
+		sys_write(*(p+5),(const void *)(*(p+6)), (unsigned)(*(p+7)));
+		f->eax = *(p+7);
+
+		default:
+		printf("System call not implemented yet\n");
+
+	}
+//  printf ("system call!\n");
+//  thread_exit ();
+}
+
+///:::
+void sys_exit(int status)
+{
+	thread_current()->exit_status = status;
+	printf("%s: exit status = %d\n", thread_current()->name, status);
+	thread_exit();
+}
+
+///:::
+void sys_write(int fd, const void* buffer, unsigned size)
+{
+	if (fd == 1)
+	{
+		putbuf(buffer, size);
+	}
+	else
+	{
+		printf("Not supposed to print anywhere else for now\n");
+	}
+}
+
+///:::
+void check_address_validity(const void* vaddr)
+{
+	if (vaddr==NULL || !is_user_vaddr(vaddr))
+		sys_exit(-1);
+	void *ptr = pagedir_get_page(thread_current()->pagedir, vaddr);
+	if (!ptr) 
+		sys_exit(-1);
 }
